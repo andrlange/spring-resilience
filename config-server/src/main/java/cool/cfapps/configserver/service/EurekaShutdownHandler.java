2d59bccbe8cbdf425a8c6765a43009fef5c92893
Andreas <andr.lange@web.de>
2024-04-14 11:31:33 +0200

    Added Micrometer distributed tracing and Zipkin

---
+++ config-server/src/main/java/cool/cfapps/configserver/service/EurekaShutdownHandler.java
@@ -0,0 +1,30 @@
+package cool.cfapps.configserver.service;
+
+import com.netflix.discovery.EurekaClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+
+public class EurekaShutdownHandler implements ApplicationListener<ContextClosedEvent> {
+
+    private final EurekaClient eurekaClient;
+
+    public EurekaShutdownHandler(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        try {
+            // Inform Eureka server about the shutdown
+            eurekaClient.shutdown();
+        } catch (Exception e) {
+            // Log the exception during Eureka deregistration
+            log.error("Failed to deregister with Eureka: " + e.getMessage());
+        }
+    }
+}
---
+++ config-server/src/test/java/cool/cfapps/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.cfapps.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
