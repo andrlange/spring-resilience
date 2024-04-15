e14439291019b7690cb40ac9c8950802b238ce06
Andreas <andr.lange@web.de>
2024-04-15 13:17:54 +0200

    added rate limiting config to address service

--- address-service/src/main/java/cool/cfapps/addressservice/controller/AddressController.java
+++ address-service/src/main/java/cool/cfapps/addressservice/controller/AddressController.java
@@ -3,6 +3,7 @@ package cool.cfapps.addressservice.controller;
 import cool.cfapps.addressservice.dto.AddressRequest;
 import cool.cfapps.addressservice.dto.AddressResponse;
 import cool.cfapps.addressservice.service.AddressService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.micrometer.observation.annotation.Observed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class AddressController {
     }
 
     @GetMapping()
-    public List<AddressResponse> getAllAddresses(){
+    public List<AddressResponse> getAllAddresses() {
         return addressService.findAll();
     }
 
@@ -31,27 +32,37 @@ public class AddressController {
     @Observed(
             name = "user.name",
             contextualName = "address-->database",
-            lowCardinalityKeyValues = {"userType","userType2"}
+            lowCardinalityKeyValues = {"userType", "userType2"}
     )
-    public ResponseEntity<AddressResponse> getAddressById(@PathVariable Long id){
-        log.info("get address with id: {}",id);
+    @RateLimiter(name = "addressLimit", fallbackMethod = "addressErrorResponse")
+    public ResponseEntity<AddressResponse> getAddressById(@PathVariable Long id) {
+        log.info("get address with id: {}", id);
         Optional<AddressResponse> result = addressService.findById(id);
         return result.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    public ResponseEntity<AddressResponse> addressErrorResponse(@PathVariable Long id, Throwable th) {
+        log.info("fallback on rate limit hit");
+        Optional<AddressResponse> result = Optional.of(AddressResponse.builder()
+                .failure(true)
+                .build());
+        return result.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+
     @PostMapping
-    public ResponseEntity<AddressResponse> createAddress(@RequestBody AddressRequest addressRequest){
+    public ResponseEntity<AddressResponse> createAddress(@RequestBody AddressRequest addressRequest) {
         log.info(addressRequest.toString());
         return addressService.create(addressRequest).map(ResponseEntity::ok).orElse(ResponseEntity.badRequest().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> deleteAddress(@PathVariable Long id){
+    public ResponseEntity<Boolean> deleteAddress(@PathVariable Long id) {
         return addressService.delete(id) ? ResponseEntity.ok(true) : ResponseEntity.notFound().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<AddressResponse> updateAddress(@PathVariable Long id, @RequestBody AddressRequest addressRequest){
+    public ResponseEntity<AddressResponse> updateAddress(@PathVariable Long id, @RequestBody AddressRequest addressRequest) {
         return addressService.update(id, addressRequest).map(ResponseEntity::ok).orElse(ResponseEntity.badRequest().build());
     }
 }
