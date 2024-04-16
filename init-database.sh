66b0690f2357725ff7c2974ab0a0fe8da13d5eb5
Andreas <andr.lange@web.de>
2024-04-16 16:04:23 +0200

    Graphana and Promoetheus

--- init-database.sh
+++ init-database.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE student;
+    CREATE DATABASE students;
     CREATE DATABASE addresses;
 EOSQL
